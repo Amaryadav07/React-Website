22a2a66ae857eefed2e8371bb42206657e9954b7
Amaryadav07 <anath2893007@gmail.com>
2025-05-06 17:56:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>My React App</h1>
+            <p>This is a simple React application.</p>
+        </div>
+    );
+}
+export default App;
